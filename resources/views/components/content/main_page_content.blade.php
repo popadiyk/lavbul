8021567df0f04c96803f3c6d318ee48a48c1aa76
popadiyk <apopadiyk@gmail.com>
2017-05-25 23:53:42 +0300

    [issue10]Create LiteVers Design in MainPage

--- resources/views/components/content/main_page_content.blade.php
+++ resources/views/components/content/main_page_content.blade.php
@@ -1,3 +1,8 @@
-<div class="col-xs-7" style="height: 500px; background: yellow;">
-    Content
+<div class="col-xs-7">
+    <div class="row head" style="height: 350px; background: yellow;">
+        Content (There slider)
+    </div>
+    <div class="row foo" style="height: 150px; background: greenyellow;">
+        Content (Карусель)
+    </div>
 </div>
--- resources/views/components/header/main_page_header.blade.php
+++ resources/views/components/header/main_page_header.blade.php
@@ -1,2 +1,43 @@
-<div class="col-xs-12 header">
+<header>
+    <div class="parallax-1">
+        <div class="parallax-2">
+            <div class="parallax-3">
+                <div class="logo"></div>
+            </div>
+        </div>
+    </div>
+    <div class="soc-network">
+        <div class="vk"><img src="img/vk.png" height="50px" width="50px"></div>
+        <div class="fb"><img src="img/fb.png" height="50px" width="50px"></div>
+        <div class="insta"><img src="img/insta.png" height="48px" width="48px"></div>
+    </div>
+    <div class="login">
+        <a href="#">Войти</a> |
+        <a href="#">Регистрация</a>
+    </div>
+    <div class="basket">
+        <a href="#">Корзина: 11550.15 грн</a>
+    </div>
+</header>
+
+<div class="row menu">
+    <a href="/" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-default">
+        Головна
+    </a>
+    <a href="#" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-info">
+        Магазин
+    </a>
+    <a href="#" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-default">
+        Новини
+    </a>
+    <a href="#" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-info">
+        Майстер-класи
+    </a>
+    <a href="{{ url('/contacts') }}" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-default">
+        Контакти
+    </a>
+    <a href="{{ url('/feedback') }}" type="button" class="col-xs-12 col-sm-4 col-md-2 mybtn btn btn-info">
+        Відгуки
+    </a>
+
 </div>
