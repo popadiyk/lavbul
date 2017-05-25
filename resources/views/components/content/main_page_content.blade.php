37cb261a394f6f6e76e0db365c15b2de1c11c85c
popadiyk <apopadiyk@gmail.com>
2017-05-25 16:58:45 +0300

    [issue8] Added marking at mainpage

---
+++ resources/views/components/content/main_page_content.blade.php
@@ -0,0 +1,3 @@
+<div class="col-xs-7" style="height: 500px; background: yellow;">
+    Content
+</div>
---
+++ resources/views/components/right_sidebar/main_page_right_sidebar.blade.php
@@ -0,0 +1,3 @@
+<div class="col-xs-2" style="height: 500px; background: lightyellow;">
+    Right_Sidebar
+</div>
