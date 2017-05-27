7b7f9c1ac66e1cb210b9fd22f675544f9765a642
SheseR <basenko.mykhaylo@hotmail.com>
2017-05-27 19:59:15 +0300

    clear migration1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,5 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(RolesTableSeeder::class);
     }
 }
