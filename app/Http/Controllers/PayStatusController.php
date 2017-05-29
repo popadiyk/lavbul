2deefbd701eeb65bde01904e0aed7f16f1e9b422
Vitaliy_Bohdaniuk <v.bogdanuk@gmail.com>
2017-05-29 16:38:48 +0000

    add models and controllers

---
+++ app/Http/Controllers/PayStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PayStatus;
+use Illuminate\Http\Request;
+
+class PayStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PayStatus  $payStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PayStatus $payStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PayStatus  $payStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PayStatus $payStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PayStatus  $payStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PayStatus $payStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PayStatus  $payStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PayStatus $payStatus)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PercentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Percent;
+use Illuminate\Http\Request;
+
+class PercentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Percent  $percent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Percent $percent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Percent  $percent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Percent $percent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Percent  $percent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Percent $percent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Percent  $percent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Percent $percent)
+    {
+        //
+    }
+}
