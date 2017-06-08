fabfdc6653d8222fe93ee6ad0a7dc3d9f4ce4915
SheseR <basenko.mykhaylo@hotmail.com>
2017-06-08 21:43:21 +0300

    [#issue_72]add formater of money for Invoice, Product, ProductMove models

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\ProductMove;
 use Illuminate\Http\Request;
 use App\Invoice;
 use Carbon\Carbon;
@@ -12,16 +13,8 @@ class TestController extends Controller
 {
    public function index()
    {
-       $invoices = Invoice::ofType(Invoice::TYPE_SALES)
-           ->ofStatus(Invoice::STATUS_CONFIRMED)
-           ->isTimeToFailed()
-           ->get();
+      $product = ProductMove::find(1);
 
-       if(count($invoices) > 0) {
-           MakerOrder::returnedProductToBase($invoices);
-       }
-
-       MakerOrder::
-       dd('flag');
+      dd(number_format($product->sum, 2));
    }
 }
