2467be7567829461fcb0aa6f5e80e5e0f3a92018
VitaliyBogdanuk <v.bogdanuk@gmail.com>
2017-07-08 13:17:21 +0300

    add new categories

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,161 @@
 @extends('layouts.main')
 @section('content')
 @include('products.header')
+<style>
+.accordion dl,
+.accordion-list {
+   border:1px solid #ddd;
+   &:after {
+       content: "";
+       display:block;
+       height:1em;
+       width:100%;
+       background-color:darken(#38cc70, 10%);
+     }
+}
+.accordion dd,
+.accordion__panel {
+   background-color:#eee;
+   font-size:1em;
+   line-height:1.5em; 
+}
+.accordion p {
+  padding:1em 2em 1em 2em;
+}
+
+.accordion {
+    position:relative;
+    background-color:#eee;
+}
+.accordionTitle,
+.accordion__Heading {
+ background-color:#38cc70; 
+   text-align:center;
+     font-weight:700; 
+          padding: 5px;
+          display:block;
+          text-decoration:none;
+          color:#fff;
+          transition:background-color 0.5s ease-in-out;
+  border-bottom:1px solid darken(#38cc70, 5%);
+  &:before {
+   content: "+";
+   font-size:1.5em;
+   line-height:0.5em;
+   float:left; 
+   transition: transform 0.3s ease-in-out;
+  }
+  &:hover {
+    background-color:darken(#38cc70, 10%);
+  }
+}
+.accordionTitleActive, 
+.accordionTitle.is-expanded {
+   background-color:darken(#38cc70, 10%);
+    &:before {
+     
+      transform:rotate(-225deg);
+    }
+}
+.accordionItem {
+    height:auto;
+    overflow:hidden; 
+    //SHAME: magic number to allow the accordion to animate
+    
+     max-height:50em;
+    transition:max-height 1s;   
+ 
+    
+    @media screen and (min-width:48em) {
+         max-height:15em;
+        transition:max-height 0.5s
+        
+    }
+    
+   
+}
+ 
+.accordionItem.is-collapsed {
+    max-height:0;
+}
+.no-js .accordionItem.is-collapsed {
+  max-height: auto;
+}
+.animateIn {
+     animation: accordionIn 0.45s normal ease-in-out both 1; 
+}
+.animateOut {
+     animation: accordionOut 0.45s alternate ease-in-out both 1;
+}
+@keyframes accordionIn {
+  0% {
+    opacity: 0;
+    transform:scale(0.9) rotateX(-60deg);
+    transform-origin: 50% 0;
+  }
+  100% {
+    opacity:1;
+    transform:scale(1);
+  }
+}
+
+@keyframes accordionOut {
+    0% {
+       opacity: 1;
+       transform:scale(1);
+     }
+     100% {
+          opacity:0;
+           transform:scale(0.9) rotateX(-60deg);
+       }
+}
+</style>
 <div class="container-fluid">
 	<div class="container">
 		<div class="row" style="margin-top: 20px; margin-bottom: 20px;">
 			<div class="col-md-3">
-				<div class="category_all">
+				 <div class="accordion">
+		            <dl>
+		              <dt>
+		                <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordion-title accordionTitle js-accordionTrigger">Accordion</a>
+		              </dt>
+		              <dd class="accordion-content accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+		              	<dl>
+			              <dt>
+			                <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordion-title accordionTitle js-accordionTrigger">First heading</a>
+			              </dt>
+			              <dd class="accordion-content accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+			               test
+			              </dd>
+			              <dt>
+			                <a href="#accordion1" aria-expanded="false" aria-controls="accordion1" class="accordion-title accordionTitle js-accordionTrigger">First Accordion heading</a>
+			              </dt>
+			              <dd class="accordion-content accordionItem is-collapsed" id="accordion1" aria-hidden="true">
+			              test
+			              </dd>
+			            </dl>
+		                
+		              </dd>
+		              <dt>
+		                <a href="#accordion2" aria-expanded="false" aria-controls="accordion2" class="accordion-title accordionTitle js-accordionTrigger">
+		                  Second heading</a>
+		              </dt>
+		              <dd class="accordion-content accordionItem is-collapsed" id="accordion2" aria-hidden="true">
+		                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu interdum diam. Donec interdum porttitor risus non bibendum. Maecenas sollicitudin eros in quam imperdiet placerat. Cras justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec dignissim arcu nec elit faucibus condimentum. Donec facilisis consectetur enim sit amet varius. Pellentesque justo dui, sodales quis luctus a, iaculis eget mauris. </p>
+		                <p>Aliquam dapibus, ante quis fringilla feugiat, mauris risus condimentum massa, at elementum libero quam ac ligula. Pellentesque at rhoncus dolor. Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor venenatis mauris placerat tristique eget id dolor. Quisque blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec elementum tincidunt.</p>
+		              </dd>
+		              <dt>
+		                <a href="#accordion3" aria-expanded="false" aria-controls="accordion3" class="accordion-title accordionTitle js-accordionTrigger">
+		                  Third Accordion heading
+		                </a>
+		              </dt>
+		              <dd class="accordion-content accordionItem is-collapsed" id="accordion3" aria-hidden="true">
+		                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu interdum diam. Donec interdum porttitor risus non bibendum. Maecenas sollicitudin eros in quam imperdiet placerat. Cras justo purus, rhoncus nec lobortis ut, iaculis vel ipsum. Donec dignissim arcu nec elit faucibus condimentum. Donec facilisis consectetur enim sit amet varius. Pellentesque justo dui, sodales quis luctus a, iaculis eget mauris. </p>
+		                <p>Aliquam dapibus, ante quis fringilla feugiat, mauris risus condimentum massa, at elementum libero quam ac ligula. Pellentesque at rhoncus dolor. Duis porttitor nibh ut lobortis aliquam. Nullam eu dolor venenatis mauris placerat tristique eget id dolor. Quisque blandit adipiscing erat vitae dapibus. Nulla aliquam magna nec elementum tincidunt.</p>
+		              </dd>
+		            </dl>
+		          </div>
+{{-- 				<div class="category_all">
 					<ul class="nav nav-pills nav-stacked" id="list_of_products">
 						<li><a href="" id="category">КАТЕГОРІЇ</a></li>
 					</ul>
@@ -46,7 +196,8 @@
 					<div class="panel">
 						<p>Lorem i</p>
 					</div>
-				</div>
+				</div> --}}
+				
 			</div>
 			<div class="col-md-9">
 				<div class="row">
