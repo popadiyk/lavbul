eca3ca8736d64e3f2fefc1e5d076e67ddc3f4456
podkopaeva <podkopaeva_yuliya@ukr.net>
2017-07-17 01:22:26 +0300

    was created structure of cabinet page

--- resources/views/cabinet/index.blade.php
+++ resources/views/cabinet/index.blade.php
@@ -1,3 +1,34 @@
+<style type="text/css">
+  a#anotherLink.nav-link {
+    background-color: #2BBBAD;
+     text-align:center;
+     font-weight:700; 
+     line-height: 1.25;
+     font-size: 0.8rem;
+     margin: 6px;
+          padding: 5px;
+          display:block;
+          text-decoration:none;
+          color:#fff;
+          transition:background-color 0.5s ease-in-out;
+  border-bottom:1px solid darken(#38cc70, 5%);
+  &:before {
+   content: "+";
+   font-size:1.5em;
+   line-height:0.5em;
+   float:left; 
+   transition: transform 0.3s ease-in-out;
+  }
+  &:hover {
+    background-color:darken(#38cc70, 10%);
+  }
+  }
+
+  a#anotherLink.nav-link.active {
+      background: #33b5e5 !important;
+  }
+</style>
+
 @extends('layouts.main')
 @section('content')
 @include('cabinet.header')
@@ -5,97 +36,50 @@
 <div class="container" style="margin-top: 30px; max-width: 960px;">
   <div class="row">
       <div class="col-md-12">
-        <!--   <nav class="nav flex-column" class="nav nav-tabs" role="tablist" style="margin-bottom: 35px;">
-          <ul>
-            <li class="nav-item">
-             <a href="#mainInfo" data-toggle="tab" role="tab" class="btn btn-default btn-sm" role="button" style="width: 250px !important; margin: 6px! important; padding: 5px !important; font-size: 0.8rem !important;">Особиста інформація</a>
-             </li>
-             <li class="nav-item">
-             <a href="#mainInfo2"  data-toggle="tab" role="tab"  class="btn btn-default btn-sm" role="button" style="width: 250px; margin: 6px; padding: 5px; font-size: 0.8rem;">Мої замовлення</a>
-             </li>
-             <li class="nav-item">
-             <a href="#" data-toggle="tab" href="#profile" role="tab" class="btn btn-default btn-sm" role="button" style="width: 250px; margin: 6px; padding: 5px; font-size: 0.8rem;">Корзина</a>
-             </li>
-          </ul>
-        </nav> -->
-        <!-- Nav tabs -->
         <div class="col-md-4 col-sm-4">
-<ul role="tablist" class="nav flex-column">
-  <li class="nav-item">
-    <a class="nav-link active" data-toggle="tab" href="#mainInfo" role="tab">Особиста інформація</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-toggle="tab" href="#profile" role="tab">Profile</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-toggle="tab" href="#messages" role="tab">Messages</a>
-  </li>
-</ul>
-</div>
-
-  
-      <!--   <div class="col-md-8" class="tab-pane active" id="mainInfo" role="tabpanel" class="tab-pane fade show active">
-          <h5>Контактні дані</h5>
-          <div class="col-md-5" style="padding: 0">
-          <p>Ім'я:</p>
-          <p>Електрона пошта:</p>
-          <p>Телефон:</p>
-          <p>Пароль:</p>
-          <p>Картка:</p>
-          <p>Знижка:</p>
-      </div>
-      <div class="col-md-6">
-        <p>yours name <i class="fa fa-pencil" aria-hidden="true"></i></p>
-        <p>yours e-mail <i class="fa fa-pencil" aria-hidden="true"></i></p>
-        <p>phone number <i class="fa fa-pencil" aria-hidden="true"></i></p>
-        <p>password <i class="fa fa-pencil" aria-hidden="true"></i></p>
-        <p>aviable</p>
-        <p>0 or percent</p>
-      </div>
-      </div> -->
-      <div class="col-md-8 col-sm-8">
-      <div class="tab-content">
-        <div class="tab-pane active" id="mainInfo" role="tabpanel">
-               <h3>Контактні дані:</h3>
-               <p>fdsfsd</p>
-               <p>sfsg</p>
-               <p>fsdfsdgfs</p>
-               <p>sdfsdg</p>
+          <ul role="tablist" class="nav flex-column">
+            <li class="nav-item">
+              <a class="nav-link active" id="anotherLink" data-toggle="tab" href="#mainInfo" role="tab" class="btn btn-default waves-effect waves-light ">Особиста інформація</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="anotherLink" data-toggle="tab" href="#history" role="tab">Moї замовлення</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" id="anotherLink" data-toggle="tab" href="#basket" role="tab">Корзина</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-8 col-sm-8">
+          <div class="tab-content">
+            <div class="tab-pane active" id="mainInfo" role="tabpanel">
+                <h4 style="margin-left: 12px;">Контактні дані</h4>
+                <div class="col-md-5 col-sm-5">
+                  <p></p>
+                  <p><strong>Ім'я:</strong></p>
+                  <p><strong>Електронна пошта:</strong></p>
+                  <p><strong>Телефон:</strong></p>
+                  <p><strong>Пароль:</strong></p>
+                  <p><strong>Картка:</strong></p>
+                  <p><strong>Знижка:</strong></p>
+                </div>
+                <div class="col-md-5 col-sm-5">
+                  <p></p>
+                   <p>yours name <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                  <p>yours e-mail <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                  <p>phone number <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                  <p>password <i class="fa fa-pencil" aria-hidden="true"></i></p>
+                  <p>aviable</p>
+                  <p>0 or percent</p>
+                </div>
+              </div>
+              <div class="tab-pane" id="history" role="tabpanel">History of your orders is empty</div>
+              <div class="tab-pane" id="basket" role="tabpanel">Your backet is empty</div>
           </div>
-        <div class="tab-pane" id="profile" role="tabpanel">fdsg</div>
-        <div class="tab-pane" id="messages" role="tabpanel">..dsfsd.</div>
-      </div>
-      </div>
-     </div>
-
-
-      <!-- <div class="tab-content">
-        <div class="col-md-8" class="tab-pane active" id="mainInfo2" role="tabpanel">
-            <h5>Контактні дані</h5>
-            <div class="col-md-5" style="padding: 0">
-            <p>Ім'я:</p>
-            <p>Електрона пошта:</p>
-            <p>Телефон:</p>
-            <p>Пароль:</p>
-            <p>Картка:</p>
-            <p>Знижка:</p>
         </div>
-        <div class="col-md-6">
-          <p>yours name <i class="fa fa-pencil" aria-hidden="true"></i></p>
-          <p>yours e-mail <i class="fa fa-pencil" aria-hidden="true"></i></p>
-          <p>phone number <i class="fa fa-pencil" aria-hidden="true"></i></p>
-          <p>password <i class="fa fa-pencil" aria-hidden="true"></i></p>
-          <p>aviable</p>
-          <p>0 or percent</p>
-        </div>
-        </div>
-      </div> -->
+    </div>
   </div>
 </div>
 
- <script type="text/javascript">
-$('#mainInfo a[href="#profile"]').tab('show');
- </script>
 
 
  @endsection
