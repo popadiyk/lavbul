c747c9a6020b2fed330d39886d7423f358d0fa53
popadiyk <apopadiyk@gmail.com>
2017-07-20 23:42:05 +0300

    Task Done Without Seeds

---
+++ app/News.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $table = 'news';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'main_photo',
+        'keyword'
+    ];
+}
