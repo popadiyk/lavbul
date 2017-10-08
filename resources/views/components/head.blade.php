73fa542537704e212540a4127f5f07c43e60a07d
popadiyk <apopadiyk@gmail.com>
2017-10-08 18:08:52 +0300

    VK Share (mobile)

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -39,7 +39,7 @@
   <script src={{ asset('js/tether.min.js') }}></script>
   <script src={{ asset('js/bootstrap.min.js') }}></script>
   <script src={{ asset('js/swiper.jquery.min.js')}}></script>
-  <script type="text/javascript" src={{ asset('js//share.js?93')}} charset="windows-1251"></script>
+  <script type="text/javascript" src={{ asset('js/share.js')}} charset="windows-1251"></script>
 
   <style>
       #our_location{
